ed8e9e8987bab00af66bba16a8525bbfdf242ff0
kapilmaurya <kapilmaurya323@gmail.com>
2023-03-07 09:04:00 +0530

    adding firebase and login setup

--- src/firebase.js
+++ src/firebase.js
@@ -19,3 +19,18 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+signInWithEmail()
+const auth = getAuth();
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,15 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import useAuth from '../../hooks/userAuth'
+import { useNavigate } from "react-router-dom";
 export default function HomePage() {
+  const {user}=useAuth();
+  const navigate=useNavigate();
+  useEffect(()=>{
+    if(!user){
+      navigate("/login")
+    }
+  },[user]);
+  
   return (
     <div >
         <h1 className="text-5xl font-bold underline">
